13c79ee0965f12556e853b65039fad266dd082c5
ldiy <lorenz_reclame@outlook.be>
2021-11-30 14:48:15 +0100

    Add ssd1306 init functions

---
+++ src/drivers/ssd1306.c
@@ -0,0 +1,160 @@
+//
+// Created by Lorenz on 15/11/2021.
+//
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "../include/drivers/ssd1306.h"
+#include "../include/hardware/i2c.h"
+
+
+ssd1306_t ssd1306_init(i2c_hw_t *i2c, uint8_t address, uint8_t width, uint8_t height) {
+    ssd1306_t ssd1306 = {i2c, address, width, height};
+
+    // Turn the display off
+    ssd1306_display_on(&ssd1306, false);
+
+    // Enable charge pump
+    uint8_t cmd_list[] = {SSD1306_COMMAND_CHARGE_PUMP_SETTING, 0x14};
+    ssd1306_send_commands(&ssd1306, cmd_list, 2);
+
+    // Set horizontal addressing mode
+    ssd1306_set_memory_addressing_mode(&ssd1306, 0x0);
+
+    // Column address 127 mapped to SEG0
+    ssd1306_set_segment_remap(&ssd1306, true);
+
+    // Flip vertically
+    ssd1306_set_com_output_scan_dir(&ssd1306, true);
+
+    // Set correct OLED panel loyout
+    ssd1306_set_com_pins(&ssd1306);
+
+    // Set the duration of the pre-charge period
+    ssd1306_set_precharge_period(&ssd1306,0x1, 0xF);
+
+    // Set Vcomh deselect level?
+
+    // Deactivate scrolling
+    ssd1306_activate_scrolling(&ssd1306, false);
+
+    // Turn the display on
+    ssd1306_display_on(&ssd1306, true);
+
+    return ssd1306;
+}
+
+/**
+ * Sends a single command
+ *
+ * @param ssd1306
+ * @param command
+ */
+static inline void ssd1306_send_command(ssd1306_t *ssd1306, uint8_t command) {
+    i2c_write(ssd1306->i2c, ssd1306->address, 0x00, 1, true, false);  // Control byte Co and D/C#
+    i2c_write(ssd1306->i2c, ssd1306->address, &command, 1, false, true);
+}
+
+/**
+ * Sends len commands from the command buffer
+ *
+ * @param ssd1306
+ * @param command
+ * @param len
+ */
+static inline void ssd1306_send_commands(ssd1306_t *ssd1306, uint8_t *command, uint32_t len) {
+    i2c_write(ssd1306->i2c, ssd1306->address, 0x00, 1, true, false);  // Control byte Co and D/C#
+    i2c_write(ssd1306->i2c, ssd1306->address, command, len, false, true);
+}
+
+/**
+ * Turns the display ON or OFF
+ *
+ * @param ssd1306
+ * @param on
+ */
+void ssd1306_display_on(ssd1306_t *ssd1306, bool on) {
+    if (on)
+        ssd1306_send_command(ssd1306, SSD1306_COMMAND_DISPLAY_ON);
+    else
+        ssd1306_send_command(ssd1306, SSD1306_COMMAND_DISPLAY_OFF);
+}
+
+/**
+ * Sets the memory addressing mode
+ *
+ * @param ssd3106
+ * @param mode
+ */
+static inline void ssd1306_set_memory_addressing_mode(ssd1306_t *ssd1306, uint8_t mode) {
+    uint8_t cmd_list[2] = {SSD1306_COMMAND_SET_MEM_ADDR_MODE, mode};
+    ssd1306_send_commands(ssd1306, cmd_list, 2);
+}
+
+/**
+ * Changes the mapping between the display data column address and the segment driver
+ *
+ * @param ssd1306
+ * @param remap 1: collum address 127 is mapped to SEG0 | 0: column address 0 is mapped to SEG0
+ */
+static inline void ssd1306_set_segment_remap(ssd1306_t *ssd1306, bool remap) {
+    uint8_t command = SSD1306_COMMAND_SET_SEGMENT_REMAP | (remap ? 1 : 0);
+    ssd1306_send_command(ssd1306, command);
+}
+
+/**
+ * Sets the scan direction of the COM output (flip vertically)
+ *
+ * @param ssd1306
+ * @param remap 1: remapped mode (COM[N-1] - COM0) | 0: normal mode (COM0 - COM[N-1])
+ */
+static inline void ssd1306_set_com_output_scan_dir(ssd1306_t *ssd1306, bool remap) {
+    uint8_t command = SSD1306_COMMAND_SET_COM_OUTPUT_SCAN_DIR;
+    if (remap)
+        command |= 1<<3;
+    ssd1306_send_command(ssd1306, command);
+}
+
+/**
+ * Sets the COM signals pin configuration to match the OLED panel hardware layout
+ *
+ * @param ssd1306
+ */
+static inline void ssd1306_set_com_pins(ssd1306_t *ssd1306) {
+    // See Table 10-3: COM Pins Hardware Configuration
+
+    // 0x02 : Disable COM left/right remap + Sequential COM pin config
+    // 0x12 : Disable COM left/right remap + Alternative COM pin config
+    // 0x22 : Enable COM left/right remap + Sequential COM pin config
+    // 0x32 : Enable COM left/right remap + Alternative COM pin config
+
+    uint8_t commands[2] = {SSD1306_COMMAND_SET_COM_PINS, 0x02};
+    if(ssd1306->height == 64)
+        commands[1] = 0x12;
+    ssd1306_send_commands(ssd1306, commands, 2);
+}
+
+/**
+ * Set the duration of the pre-charge period
+ *
+ * @param ssd1306
+ * @param phase1_period Counted in number of DCLK
+ * @param phase2_period Counted in number of DCLK
+ */
+static inline void ssd1306_set_precharge_period(ssd1306_t *ssd1306, uint8_t phase1_period, uint8_t phase2_period) {
+    uint8_t commands[2] = {SSD1306_COMMAND_SET_PRECHARGE_PERIOD, (uint8_t)(phase2_period << 4) & phase1_period};
+    ssd1306_send_commands(ssd1306, commands, 2);
+}
+
+/**
+ * Activate or deactivate scrolling
+ *
+ * @param ssd1306
+ * @param activate
+ */
+static inline void ssd1306_activate_scrolling(ssd1306_t *ssd1306, bool activate) {
+    if (activate)
+        ssd1306_send_command(ssd1306, SSD1306_COMMAND_ACTIVATE_SCROLL);
+    else
+        ssd1306_send_command(ssd1306, SSD1306_COMMAND_DEACTIVATE_SCROLL);
+}
---
+++ src/include/drivers/ssd1306.h
@@ -0,0 +1,54 @@
+//
+// Created by Lorenz on 15/11/2021.
+//
+
+#ifndef KEYLOGGER_SSD1306_H
+#define KEYLOGGER_SSD1306_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "../hardware/i2c.h"
+
+// See SSD1306 datasheet
+typedef struct {
+    i2c_hw_t *i2c;
+    const uint8_t address;
+    const uint8_t width;
+    const uint8_t height;
+}ssd1306_t;
+
+// Fundamental Commands (See Table 9-1-1)
+#define SSD1306_COMMAND_DISPLAY_OFF 0xAE
+#define SSD1306_COMMAND_DISPLAY_ON  0xAF
+// Scrolling (See Table 9-1-2)
+#define SSD1306_COMMAND_DEACTIVATE_SCROLL   0x2E
+#define SSD1306_COMMAND_ACTIVATE_SCROLL     0x2F
+// Timing & driving scheme setting (See Table 9-1-5)
+#define SSD1306_COMMAND_SET_DISPLAY_CLK         0xDF
+#define SSD1306_COMMAND_SET_PRECHARGE_PERIOD    0xD9
+// Hardware configuration (See Table 9-1-4)
+#define SSD1306_COMMAND_SET_MULTIPLEX_RATIO     0xA8
+#define SSD1306_COMMAND_SET_DISPLAY_OFFSET      0xD3
+#define SSD1306_COMMAND_SET_DISPLAY_START_LINE  0x40  // Uses bit 5-0 for data
+#define SSD1306_COMMAND_SET_SEGMENT_REMAP       0xA0
+#define SSD1306_COMMAND_SET_COM_OUTPUT_SCAN_DIR 0xC0
+#define SSD1306_COMMAND_SET_COM_PINS            0xDA
+// Addressing setting (See Table 9-1-3)
+#define SSD1306_COMMAND_SET_MEM_ADDR_MODE   0x20
+// Charge bumb setting (See Table 2.1-1 Application note)
+#define SSD1306_COMMAND_CHARGE_PUMP_SETTING 0x8D
+
+// Function prototypes
+ssd1306_t ssd1306_init(i2c_hw_t *i2c, uint8_t address, uint8_t width, uint8_t height);
+static inline void ssd1306_send_command(ssd1306_t *ssd1306, uint8_t command);
+static inline void ssd1306_send_commands(ssd1306_t *ssd1306, uint8_t *command, uint32_t len);
+void ssd1306_display_on(ssd1306_t *ssd1306, bool on);
+static inline void ssd1306_set_memory_addressing_mode(ssd1306_t *ssd1306, uint8_t mode);
+static inline void ssd1306_set_segment_remap(ssd1306_t *ssd1306, bool remap);
+static inline void ssd1306_set_com_output_scan_dir(ssd1306_t *ssd1306, bool remap);
+static inline void ssd1306_set_com_pins(ssd1306_t *ssd1306);
+static inline void ssd1306_set_precharge_period(ssd1306_t *ssd1306, uint8_t phase1_period, uint8_t phase2_period);
+static inline void ssd1306_activate_scrolling(ssd1306_t *ssd1306, bool activate);
+
+
+#endif //KEYLOGGER_SSD1306_H
